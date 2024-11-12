5e40991bf98060e29e3f94648790fcfe79b47815
Ben <bmaduabuchi02@gmail,com>
2024-11-11 20:47:02 -0700

    lint fix

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -3,7 +3,6 @@ const config = require("../config.js");
 const { Role, DB } = require("../database/database.js");
 const { authRouter } = require("./authRouter.js");
 const { asyncHandler, StatusCodeError } = require("../endpointHelper.js");
-const measurePizzaLatency = require("../measureLatency.js");
 const metrics = require("../metrics");
 
 const orderRouter = express.Router();
